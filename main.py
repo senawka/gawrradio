3702b89fe99aeda2a32a601fe3743666307cca69
sen <89331666+senawka@users.noreply.github.com>
2023-07-24 09:10:26 -0400

    Update main.py
    reflects all changes for .json

--- main.py
+++ main.py
@@ -3,16 +3,24 @@
 import discord
 from discord.ext import commands
 import asyncio
+import json
 import random
 
-with open('token.txt') as t:
-    token = t.read()
+def load_config():
+    with open('config.json') as config_file:
+        return json.load(config_file)
 
 async def start_bot():
     bot = commands.Bot(command_prefix=',', intents=discord.Intents.all(), help_command=None)
+    config = load_config()
+    bot.config = config
     await bot.load_extension('cogs.music_cog')
     return bot
 
 if __name__ == '__main__':
-    bot = asyncio.run(start_bot())
-    bot.run(f'{token}')
+    loop = asyncio.get_event_loop()
+    bot = loop.run_until_complete(start_bot())
+
+    with open('token.txt') as t:
+        token = t.read()
+    bot.run(token)
