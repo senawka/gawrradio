ff10e846c9964ad409fcc4565c8a04cc99434be0
sen <89331666+senawka@users.noreply.github.com>
2023-07-24 08:23:41 -0400

    Update main.py
    remove help

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ with open('token.txt') as t:
 async def start_bot():
     bot = commands.Bot(command_prefix=',', intents=discord.Intents.all(), help_command=None)
     await bot.load_extension('cogs.music_cog')
-    await bot.load_extension('cogs.help_cog')
     return bot
 
 if __name__ == '__main__':
